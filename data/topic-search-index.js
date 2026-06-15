4ad998c655504b9d80f780837c2e5cacd1ef28cc
yuzhou4t <aaawdeewfjfjfjfjfj@gmail.com>
2026-06-15 10:46:50 +0800

    chore(data): publish local daily articles 2026-06-15

--- data/topic-search-index.js
+++ data/topic-search-index.js
@@ -1,8 +1,8 @@
 window.TOPIC_SEARCH_INDEX = {
   "version": 1,
-  "updated_at": "2026-06-13T11:41:48.346Z",
+  "updated_at": "2026-06-15T02:45:36.074Z",
   "summary": {
-    "total_articles": 455,
+    "total_articles": 467,
     "matched_articles": 3,
     "topics": 1,
     "disciplines": 7,
