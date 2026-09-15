d4119c8ab19f64a7f1a7cad4ce348ded6f9253dc
yuzhou4t <aaawdeewfjfjfjfjfj@gmail.com>
2026-09-15 11:00:29 +0800

    chore(data): publish daily articles 2026-09-15

--- data/topic-search-index.js
+++ data/topic-search-index.js
@@ -1,8 +1,8 @@
 window.TOPIC_SEARCH_INDEX = {
   "version": 1,
-  "updated_at": "2026-09-12T03:00:28.358Z",
+  "updated_at": "2026-09-15T03:00:28.922Z",
   "summary": {
-    "total_articles": 1019,
+    "total_articles": 1045,
     "matched_articles": 7,
     "topics": 1,
     "disciplines": 7,
